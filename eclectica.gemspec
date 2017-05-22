85ca6940fdb11f6d7407e6ad902e170c90d975b2
Oleg Gaidarenko <markelog@gmail.com>
2017-05-22 19:03:04 +0200

    Bump version to 0.0.13

--- eclectica.gemspec
+++ eclectica.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = "eclectica"
-  s.version     = "0.0.12"
+  s.version     = "0.0.13"
   s.licenses    = "MIT"
   s.author      = "Oleg Gaidarenko"
   s.email       = "markelog@gmail.com"
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 from setuptools import setup, find_packages
 
-version = '0.0.12'
+version = '0.0.13'
 
 def download():
   _, out = getstatusoutput('which pip')
