d9b771fe14e8c90295cc67710185f292601879a8
Oleg Gaidarenko <markelog@gmail.com>
2021-09-07 14:02:29 +0400

    Bump version to 0.8.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools.command.develop import develop
 from setuptools.command.install import install
 from setuptools import setup, find_packages
 
-version = '0.7.1'
+version = '0.8.0'
 
 def download():
   p = Popen(["which", "ec"], stdout=PIPE, stderr=PIPE)
